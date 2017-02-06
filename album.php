1b0d07fd347c7d3b6cb85aad4acfda6404364e8a
Denis MOUREU-LARRANG <dmoureu-@e1r9p20.42.fr>
2017-02-06 20:29:50 +0100

    OK

--- album.php
+++ album.php
@@ -11,26 +11,31 @@ if ($_GET['action'] == "delete" && $_GET['id'] > 0) {
 
 if ($_GET['action'] == "view" && $_GET['id'] > 0) {
   $id = intval($_GET['id']);
-  echo "<div class='view'>";
-  echo   '<img src="./rendu/pict-'.$id.'.jpeg">';
+  echo '<div class="view decobox">
+  <div class="imgfull" ">'; //style="background-image:url(\'./rendu/pict-'.$id.'.jpeg\');
+  echo   '<img src="./rendu/pict-'.$id.'.jpeg"></div>';
   include "layout/form/com.php";
   echo "</div>";
 }
 
 echo '<p>Galery</p>';
 
+echo '<div class="gallery decobox">';
 $sql = "SELECT * FROM img";
 foreach  ($db->query($sql) as $row) {
 
 	echo '<div class="thumb">
-          <a href="./index.php?page=gallery&action=view&id='.$row['id'].'">
-            <img src="./rendu/pict-'.$row['id'].'.jpeg">';
+
+          <div class="pict" style="background-image:url(\'./rendu/pict-'.$row['id'].'.jpeg\');" onclick="document.location=\'./index.php?page=gallery&action=view&id='.$row['id'].'\'">
+            ';//<img src="./rendu/pict-'.$row['id'].'.jpeg">
                     	if ($row['uid'] == $_SESSION['logged_id']) {
                     		echo '<a class="del" href="index.php?page=gallery&action=delete&id='.$row['id'].'">
                     			<img src="./resources/delete.png">
                     		</a>';
                     	}
-  echo '   </a>
+  echo '   </div>
         </div>';
 }
 ?>
+
+</div>
